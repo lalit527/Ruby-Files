28964d53dc5eb1491b7cb3a93c3131ccadea4dfc
lalit527 <lalit.slg007@gmail.com>
2018-05-04 15:15:50 +0530

    Added Proc to preserve scope of variable

--- Well-Grounded-Rubyist/ruby-dynamics/chapter14/14.1/proc-closure-14.1.5.rb
+++ Well-Grounded-Rubyist/ruby-dynamics/chapter14/14.1/proc-closure-14.1.5.rb
@@ -7,9 +7,20 @@ a = "Goodbye"
 talk
 puts a
 
+## The identifier a has been assigned to twice, but the two assignments are unrelated to each other.
+
+
+m = 10 
+[1,2,3].each {|x| puts x * m }
+
+## that code blocks preserve the variables that were in existence at the time they were created
+
 def multiply_by(m) 
 	Proc.new {|x| puts x * m } 
 end
 
 mult = multiply_by(10) 
 mult.call(12)
+
+## The variable m value is preserved
+## and serves as multiplier to Proc object returned from multiply_by is called.
