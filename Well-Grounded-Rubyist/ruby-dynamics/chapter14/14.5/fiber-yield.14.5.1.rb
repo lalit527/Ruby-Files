15c4f0f4fd82004efc7401e5cb61163cfbab5987
lalit527 <lalit.slg007@gmail.com>
2018-05-07 14:40:58 +0530

    Adding FIber

---
+++ Well-Grounded-Rubyist/ruby-dynamics/chapter14/14.5/fiber-yield.14.5.1.rb
@@ -0,0 +1,14 @@
+f = Fiber.new do
+  puts "Hi."
+  Fiber.yield
+  puts "Nice day."
+  Fiber.yield
+  puts "Bye!"
+end
+
+f.resume 
+puts "Back to the fiber:"
+f.resume 
+puts "One last message from the fiber:"
+f.resume 
+puts "That's all!"
