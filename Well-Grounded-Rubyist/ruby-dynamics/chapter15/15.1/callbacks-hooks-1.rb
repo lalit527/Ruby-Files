c0caa5a6aba7f60e497fda34df9cdd8a8dcfbc8d
lalit527 <lalit.slg007@gmail.com>
2018-05-08 13:07:59 +0530

    Added program for callbacks and hooks

---
+++ Well-Grounded-Rubyist/ruby-dynamics/chapter15/15.1/callbacks-hooks-1.rb
@@ -0,0 +1,10 @@
+"""
+An event is something like
+A nonexistent method being called on an object 
+A module being mixed in to a class or another module 
+An object being extended with a module 
+A class being subclassed (inherited from) 
+A reference being made to a nonexistent constant 
+An instance method being added to a class 
+A singleton method being added to an object
+"""
