ff87195da813f9358ed211857d7bececa6f0c5cd
lalit527 <lalit.slg007@gmail.com>
2018-05-10 20:13:20 +0530

    Added singleton methods

--- Well-Grounded-Rubyist/ruby-dynamics/chapter15/15.2/singleton-methods.4.rb
+++ Well-Grounded-Rubyist/ruby-dynamics/chapter15/15.2/singleton-methods.4.rb
@@ -22,3 +22,14 @@ p c.singleton_methods.sort
 class << c
   p private_instance_methods(false)
 end
+
+puts File.singleton_methods - File.singleton_methods(false)
+
+"""
+call to singleton_methods(false) provides only the singleton methods defined on File
+The call without the false argument provides all the singleton methods defined on File and its ancestors.
+"""
+
+## The superclass of File is IO.
+
+puts IO.singleton_methods(false)
