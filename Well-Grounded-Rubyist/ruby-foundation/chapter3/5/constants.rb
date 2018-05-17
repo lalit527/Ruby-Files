44f0b105af6af0d44bb9af83409dc327f54a814f
lalit527 <lalit.slg007@gmail.com>
2018-05-17 15:04:26 +0530

    Added online song using normal and scope method in the Model

---
+++ Well-Grounded-Rubyist/ruby-foundation/chapter3/5/constants.rb
@@ -0,0 +1,12 @@
+'''
+In Ruby constants are defined with capital letter
+'''
+
+class Ticket 
+  VENUES = ["Convention Center", "Fairgrounds", "Town Hall"] 
+end
+
+"""
+It’s possible to perform an assignment on a constant to which you’ve already assigned something—that is, to reassign to the constant. 
+But you get a warning if you do this
+"""
