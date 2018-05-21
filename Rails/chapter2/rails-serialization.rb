2a8797a58029ce100868bd39cc01e0efd14287dc
lalit527 <lalit.slg007@gmail.com>
2018-05-21 16:29:05 +0530

    added serialization in rails

---
+++ Rails/chapter2/rails-serialization.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+  has_many :referral_types
+end
+
+class Referral < ActiveRecord::Base 
+  has_and_belongs_to_many :users 
+end
+
+## Using Has many
+class User < ActiveRecord::Base
+  has_many :referral_types 
+end
+
+class Referral < ActiveRecord::Base
+  VALUES = ['Newsletter', 'School', 'Web', 'Partners/Events', 'Media', 'Other'] 
+  validates :value, :inclusion => {:in => VALUES}
+  
+  belongs_to :user 
+end
+
+## Using Serialize
+class User < ActiveRecord::Base 
+  HEARD_THROUGH_VALUES = ['Newsletter', 'School', 'Web', 'Partners/Events', 'Media', 'Other']
+
+  serialize :heard_through, Hash 
+end
