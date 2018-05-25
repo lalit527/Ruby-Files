5dae029365a82ece3c9004d3fab5986fc0e41efa
lalit527 <lalit.slg007@gmail.com>
2018-05-25 14:26:50 +0530

    Added Ruby Iteration

--- Well-Grounded-Rubyist/ruby-foundation/chapter6/6.1/basic.rb
+++ Well-Grounded-Rubyist/ruby-foundation/chapter6/6.1/basic.rb
@@ -12,3 +12,49 @@ else
   puts 'else'
 end
 """
+
+"""
+Negeting Condition:
+if not (condition1==1)
+
+if !(condition1==1)
+
+if not condition1 == 1
+
+if !condition1 == 1 ## eq to if (!condition1) == 1
+"""
+
+"""
+
+"""
+x = 1
+
+unless x == 2
+  puts "cool"
+else 
+  puts "check"
+end
+
+puts "======================================================"
+if false
+  z = 2
+end
+p z # print nil
+
+puts "======================================================"
+if x = 1
+  puts "Hi!"
+end
+# warning: found = in conditional, should be ==
+# if x = y No warning as both sides are variable
+
+## Case Statement
+puts "======================================================"
+
+case 
+  when user.first_name == "David", user.last_name == "Black"
+    puts "You might be David Black." 
+  when Time.now.wday == 5
+    puts "You're not David Black, but at least it's Friday!" 
+  else
+    puts "You're not David Black, and it's not Friday." end
